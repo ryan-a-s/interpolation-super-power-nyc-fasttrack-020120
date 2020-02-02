affa43db2bf0e73862702b483e0e13ac718ed903
Ryan Seit <ryan@ryanseit.com>
2020-02-02 03:46:31 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,1 +1,8 @@
 # Write your #display_rainbow method here
+
+def display_rainbow(colors)
+  
+end
+
+color_array = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"]
+display_rainbow(color_array)
